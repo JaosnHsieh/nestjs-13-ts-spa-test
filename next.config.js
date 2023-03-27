aacd05a7a732aa3899ee76bb4e8995e4a2f07a34
jasonh <appleoxxo@gmail.com>
2023-03-27 18:46:46 +0800

    spa works

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:any*',
+        destination: '/',
+      },
+    ];
+  },
+};
